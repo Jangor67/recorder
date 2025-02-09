5c3ad53b93b1a6d6d4bdc39d75a23d7fbfac92bc
Jan de Gorter <Jan@deGorter.net>
2025-02-09 16:39:51 +0100

    don't use sudo to rm files

--- cleaner.sh
+++ cleaner.sh
@@ -21,7 +21,7 @@ remove_oldest_directory() {
     
     if [ -n "$COMPLETE_PATH" ]; then
         echo "Removing oldest directory: $COMPLETE_PATH"
-        sudo rm -rf "$COMPLETE_PATH"
+        rm -rf "$COMPLETE_PATH"
     else
         echo "No directories found to delete."
     fi
